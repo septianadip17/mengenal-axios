347da3a8a9f7a80f13c55a74ae63623bcbaa16e2
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-27 13:50:08 +0700

    success fetch and axios

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("mantappp")
+var url = "https://reqres.in/api/users";
+
+// ini menggunakan fetch
+// fetch(url + "/1")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data.data))
+//   .catch((error) => console.log("Error: " + error));
+
+// // fetch method post
+// fetch(url, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Wakhid",
+//     job: "Pedagang",
+//   }),
+// })
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("Error:", error));
+
+// // ini menggunakan axios
+// axios
+//   .get(url + "/1")
+//   .then((data) => console.log(data.data))
+//   .catch((error) => console.log("error: " + error));
+
+// axios method post
+axios
+  .post(url, {
+    name: "Dwi",
+    job: "Karyawan Swasta",
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
