7503abe1b182b94c5a2bf1b4cbc63dd036e50a57
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-27 13:58:03 +0700

    finish

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var url = "https://reqres.in/api/users";
 
-// ini menggunakan fetch
+// // ini menggunakan fetch
 // fetch(url + "/1")
 //   .then((response) => response.json())
 //   .then((data) => console.log(data.data))
@@ -21,17 +21,28 @@ var url = "https://reqres.in/api/users";
 //   .then((data) => console.log(data))
 //   .catch((error) => console.error("Error:", error));
 
+// // error in fetch
+// fetch(url + "/x")
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log("Error: " + error));
+
 // // ini menggunakan axios
 // axios
 //   .get(url + "/1")
 //   .then((data) => console.log(data.data))
 //   .catch((error) => console.log("error: " + error));
 
-// axios method post
-axios
-  .post(url, {
-    name: "Dwi",
-    job: "Karyawan Swasta",
-  })
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error:", error));
+// // axios method post
+// axios
+//   .post(url, {
+//     name: "Dwi",
+//     job: "Karyawan Swasta",
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("Error:", error));
+
+// // error in axios
+// axios
+//   .get(url + "/x")
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log("Error: " + error));
